0e56696fa9f58cc68125aab93c2065e4eeab8b27
aimspot <82668230+aimspot@users.noreply.github.com>
2023-08-31 15:41:00 +0300

    update: delete folder after seven days

--- date.py
+++ date.py
@@ -1,16 +1,32 @@
 import os
 import time
-from datetime import datetime
+from datetime import datetime, timedelta
 import cv2
 import argparse 
-from loguru import logger 
+from loguru import logger
+import shutil 
 
 cameras_url = ['rtsp://admin:1q2w3e4r@192.168.20.100/Streaming/Channels/101', 'rtsp://admin:1q2w3e4r@192.168.20.98/Streaming/Channels/101'] 
 
 def opt(): 
     parser = argparse.ArgumentParser() 
     parser.add_argument('--camera', type=int, default=1, help='№ camera [1, 2]') 
-    return parser.parse_args() 
+    return parser.parse_args()
+
+
+def delete_old_folders(base_path):
+    days_threshold = 8
+    today = datetime.now()
+    for folder_name in os.listdir(base_path):
+        try:
+            folder_date = datetime.strptime(folder_name, "%d_%m_%Y")
+            if (today - folder_date) > timedelta(days=days_threshold):
+                folder_path = os.path.join(base_path, folder_name)
+                if os.path.isdir(folder_path):
+                    shutil.rmtree(folder_path)
+                    print(f"Deleting folder: {folder_path}")
+        except ValueError:
+            pass
 
 
 def get_names_folders(path_root_folder): 
@@ -43,10 +59,6 @@ def main(opt):
     create_directory(date_folder)
     create_directory(time_folder)
 
-    #rtsp://admin:1q2w3e4r@192.168.20.98/Streaming/Channels/101
-    #rtsp://admin:1q2w3e4r@192.168.20.100/Streaming/Channels/101
-    #cap = cv2.VideoCapture('rtsp://admin:1q2w3e4r@192.168.20.98/Streaming/Channels/101')#98
-
     cap = cv2.VideoCapture(cameras_url[opt.camera - 1])
 
     frame_width = 1920#int(cap.get(3))
@@ -68,6 +80,7 @@ def main(opt):
 
         create_directory(date_folder)
         create_directory(time_folder)
+        delete_old_folders(path_root_folder)
 
 
         if past_time != end_time:
@@ -77,20 +90,14 @@ def main(opt):
             out = cv2.VideoWriter(output_file, fourcc, fps, (frame_width, frame_height))
 
         try:
-            #time.sleep(0.001)
             ret, frame = cap.read() 
             if not ret: 
                 cap.release() 
                 continue
             else:
-                # cv2.imshow(path_root_folder, frame)
                 out.write(frame)
         except: 
             cap.release() 
-
-        # ret, frame = cap.read()
-        
-        # out.write(frame)
 
 if __name__ == "__main__":
     opt = opt() 
